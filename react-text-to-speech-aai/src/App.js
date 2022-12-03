1792286fbaac0a103afe32431da1ac83f73f6dd5
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-03 14:33:44 +0500

    done with app

--- react-text-to-speech-aai/src/App.js
+++ react-text-to-speech-aai/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
 
 function App() {
+  // Initiate our state variable which will store the text we want read aloud
+  const [ourText, setOurText] = useState("")
+  const msg = new SpeechSynthesisUtterance()
+
+  // Create speechHandler function that takes our (msg) variable as parameter. 
+  const speechHandler = (msg) => {
+    msg.text = ourText
+    window.speechSynthesis.speak(msg)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Text to Speech App</h1>
+      <input
+        type='text'
+        value={ourText}
+        placeholder="Enter text"
+        onChange={(e) => setOurText(e.target.value)}
+      />
+      <button onClick={() => speechHandler(msg)}>Speak</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
